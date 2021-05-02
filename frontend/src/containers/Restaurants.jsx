7e2aa8a51b7fbe05653768f68036a428426e8452
tatsuro <oscarwild13@gmail.com>
2021-05-02 19:12:46 +0900

    レストラン一覧にstyled-componentsを充てる

--- frontend/src/containers/Restaurants.jsx
+++ frontend/src/containers/Restaurants.jsx
@@ -1,5 +1,30 @@
 import React, {useEffect} from 'react';
+import styled from 'styled-components';
+
+// apis
 import {fetchRestaurants} from '../apis/restaurants';
+
+// images
+import MainLogo from '../images/logo.png';
+import MainCoverImage from '../images/main-cover-image.png';
+
+const HeaderWrapper = styled.div`
+  display:flex;
+  justify-content:flex-start;
+  padding:8px 32px;
+`;
+
+const MainLogoImage = styled.img`
+  height:90px;
+`;
+
+const MainCoverImageWpper = styled.div`
+  text-align:center;
+`;
+
+const MainCover = styled.img`
+  height:600px;
+`
 
 export const Restaurants = () => {
   useEffect(() => {
@@ -10,7 +35,12 @@ export const Restaurants = () => {
   }, [])
     return(
         <>
-          レストラン一覧
+        <HeaderWrapper>
+          <MainLogoImage src={MainLogo} alt="main logo" />
+        </HeaderWrapper>
+        <MainCoverImageWpper>
+          <MainCover src={MainCoverImage} alt="main cover" />
+        </MainCoverImageWpper>
         </>
     )
 }
